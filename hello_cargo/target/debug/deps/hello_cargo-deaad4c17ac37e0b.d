04b5023b1941654cfe7a3f5804097ae8fca2708a
Denis Surina <denis.surina1@gmail.com>
2024-07-24 22:40:02 +0200

    rust hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b.d
@@ -0,0 +1,5 @@
+/home/denis/stuff/rust/rust-basics/hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b: src/main.rs
+
+/home/denis/stuff/rust/rust-basics/hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/denis/stuff/rust/rust-basics/hello_cargo/target/debug/hello_cargo: /home/denis/stuff/rust/rust-basics/hello_cargo/src/main.rs
