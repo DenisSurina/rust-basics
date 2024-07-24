10d5ba1e98a898febcb631a24e5a218308330f8a
Denis Surina <denis.surina1@gmail.com>
2024-07-25 00:26:37 +0200

    guessing game started

---
+++ guessing_game/target/debug/deps/guessing_game-8b524aa291ee096e.d
@@ -0,0 +1,5 @@
+/home/denis/stuff/rust/rust-basics/guessing_game/target/debug/deps/guessing_game-8b524aa291ee096e: src/main.rs
+
+/home/denis/stuff/rust/rust-basics/guessing_game/target/debug/deps/guessing_game-8b524aa291ee096e.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/denis/stuff/rust/rust-basics/guessing_game/target/debug/guessing_game: /home/denis/stuff/rust/rust-basics/guessing_game/src/main.rs
